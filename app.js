14cdc624e21a08a8d0ce20bf1027aefd8a9c1f85
Jay Zehngebot <jay.zehngebot@gmail.com>
2013-04-12 04:02:55 -0400

    database hooked up, submissions working, main view working

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var moment = require('moment');
+var mongoose = require('mongoose');
 
 
 // the ExpressJS App
@@ -35,7 +36,7 @@ app.configure(function(){
   app.use(express.static(path.join(__dirname, 'public')));
 
   // database - skipping until week 5
-  // app.db = mongoose.connect(process.env.MONGOLAB_URI);
+  app.db = mongoose.connect(process.env.MONGOLAB_URI);
   
 });
 
@@ -47,6 +48,8 @@ app.configure('development', function(){
 // ROUTES
 var routes = require('./routes/index.js');
 app.get('/', routes.index);
+app.get('/view', routes.view);
+app.post('/main', routes.postClass);
 
 // create NodeJS HTTP server using 'app'
 
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@
  */
 
  var siteTitle = "ITPWishList";
+ var moment = require("moment"); // date manipulation library
+var dreamCourseModel = require("../models/dreamCourse.js"); //db model
 
 exports.index = function(req, res){
 
@@ -11,4 +13,56 @@ exports.index = function(req, res){
 		title : siteTitle,
 	}
   res.render('main', templateData);
+};
+
+exports.view = function(req, res){
+
+	console.log('main page req');
+
+	dreamCourseModel.find({}, 'name description proj_or_theory ayes', function(err, allClasses){
+		if (err){
+			res.send('error with db').status(500);
+		};
+		console.log('nabbed classes');
+	
+	var templateData = {
+		classes : allClasses,
+	}
+
+  res.render('view', templateData);
+});
+}
+
+exports.postClass = function(req, res){
+
+	console.log('recieved new class');
+	console.log(req.body);
+
+var newClass = new dreamCourseModel({
+	name : req.body.classTitle,
+	slug : req.body.classTitle.toLowerCase().replace(/[^\w ]+/g,'').replace(/ +/g,'_'),
+	description : req.body.descript,
+	//themes : req.body.themes.split(","),
+	//skills : req.body.skills.split(","),
+	//genius : 
+	date : moment().format(),
+	proj_or_theory : req.body.proj_or_theory,
+	ayes : 0
+
+});
+
+newClass.save(function(err){
+		if (err) {
+			console.error("Error on saving new class");
+			console.error(err);
+			return res.send("There was an error when creating a new class");
+
+		} else {
+			console.log("Created a new dreamClass!");
+			console.log(newClass);
+			
+			// redirect to the astronaut's page
+			res.redirect('/view');
+		}
+	});
 };
